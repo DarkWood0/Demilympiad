cd964a1b5462cfa741a2c067190ffc45ec9eb227
Aleksey <darkwood0@gmail.com>
2018-03-02 18:23:04 +0300

    Текст голосований
    Добавлен дополнительный скрипт для облегчения оформления голосований

---
+++ categories_template.py
@@ -0,0 +1,12 @@
+categories_text = """растровая графика, векторная графика, живые материалы, фотография, фотоарт, трехмерное моделирование, дпи, фрактал-арт"""
+categories = categories_text.split(', ')
+for category in categories:
+  if category == 'дпи':
+    category = category.upper()
+  else:
+    category = category.capitalize()
+  text = '[c][IMG]http://demiart.ru/forum/uploads20/post-1120952-1515957359-5a5bac6fcad85.png[/IMG]\n'
+  text += '[COLOR=orange][SIZE=7]Голосование по второму этапу в категории "' + category + '"[/SIZE][/COLOR]\n'
+  text += '[COLOR=yellow][SIZE=7]С 14.02 по 18.02 включительно[/SIZE][/COLOR][/C]\n\n'
+  text += 'Выберите три лучших здания.'
+  print(text)
